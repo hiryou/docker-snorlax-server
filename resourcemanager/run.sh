c2706784a508a2141511d310089983544074829c
Long Nguyen <longn@zillowgroup.com>
2020-06-24 03:20:02 -0700

    Fix to bring up resource manager

--- resourcemanager/run.sh
+++ resourcemanager/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager
+#sleep 60
+#${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager
+
+n=0
+until [ "$n" -ge 5 ]
+do
+   ${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager && break  # substitute your command here
+   n=$((n+1))
+   sleep 10
+done
