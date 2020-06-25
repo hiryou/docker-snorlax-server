1dd6c305395e65b0c3fe342697fad025fc680904
Long Nguyen <longn@zillowgroup.com>
2020-06-24 21:37:36 -0700

    adding spark, working.......

---
+++ spark-on-yarn/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#sleep 60
+#${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager
+
+n=0
+until [ "$n" -ge 5 ]
+do
+   ${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager && break  # substitute your command here
+   n=$((n+1))
+   sleep 10
+done
